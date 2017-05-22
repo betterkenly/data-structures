8837eaf74dce6385a186d392d1408e36394f323d
betterkenly <betterkenly@gmail.com>
2017-05-21 17:20:42 -0700

    added_properties

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,6 +1,7 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
+  newTree.parent = null;
 
   // your code here
   newTree.children = [];  // fix me
@@ -12,6 +13,7 @@ var treeMethods = {};
 
 treeMethods.addChild = function(value) {
 	var newChild = Tree(value);
+	newChild.parent = this;
 	this.children.push(newChild);
 };
 // debugger;
@@ -33,10 +35,33 @@ treeMethods.contains = function(target) {
 	return search(this) === true? true: false;
 };
 
+treeMethods.removeFromParent = function(target){
+	var searchChild = function(tree){
 
-var tree = Tree(5);
-tree.addChild(5);
-tree.addChild(6);
+		for (var i = 0; i < tree.children.length; i++) {
+			if (tree.children[i].value === target) {
+				tree.children[i].parent = null;
+				tree.children.splice(i,1);
+
+			} else {
+				searchChild(tree.children[i]);
+			}
+		}
+	}
+	searchChild(this);
+}
+		//1
+	// 2      3
+ //  7       8   9
+
+
+var tree = Tree(1);
+debugger;
+tree.addChild(2);
+tree.addChild(3);
 tree.children[0].addChild(7);
 tree.children[1].addChild(8);
-tree.contains(7);
+tree.children[1].addChild(9);
+tree.removeFromParent(9);
+
+tree.contains(9);
