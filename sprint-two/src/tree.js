222f2f897d277e96441064cb4b116c4ee1a5d8ca
betterkenly <betterkenly@gmail.com>
2017-05-21 17:51:49 -0700

    tree_added_traverse

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -50,18 +50,29 @@ treeMethods.removeFromParent = function(target){
 	}
 	searchChild(this);
 }
+
+treeMethods.traverse = function(callback){
+	var search = function(tree){
+		callback(tree);
+		for (var i = 0; i < tree.children.length ; i++) {
+			search(tree.children[i]);
+		}
+	};
+	search(this);
+}
 		//1
 	// 2      3
  //  7       8   9
 
-
-var tree = Tree(1);
 debugger;
+var tree = Tree(1);
 tree.addChild(2);
 tree.addChild(3);
 tree.children[0].addChild(7);
 tree.children[1].addChild(8);
 tree.children[1].addChild(9);
-tree.removeFromParent(9);
-
+// tree.removeFromParent(9);
 tree.contains(9);
+var func = function(x){ return x.value = x.value * 2};
+tree.traverse(func);
+console.log(tree.contains(18));
